13a37389d8638397082d7761eed3214f2e9d1326
Damir <damircszar@mail.ru>
2025-07-16 16:53:02 +0300

    добавил контента, еще не закончил

---
+++ src/Components/Section.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Section = () => {
+    return (
+      <section className='content'>
+        <ul className='snkrs-list'>
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/9d3/iq70235n21xg7qja88zfj41ncwibmwhm/2160_2160_1/NKHJ4497400.webp'></img></li>      
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/e73/kl7lkrah49y0iolkkqmeztrqnbmda0y8/2160_2160_1/AN8125177194.webp'></img></li>  
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/d29/j4w2uddggv3xmjb32v26mqy0f4swya2h/2160_2160_1/NBU1906RCB.webp'></img></li>
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/ec2/41fx71o654q98vfilmt0ks4aech705ce/2160_2160_1/NBU9060AAB.webp'></img></li>      
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/9a7/w219j30k5eoyp31303q4u8j8ocfbi5h4/2160_2160_1/NKHQ3818001.webp'></img></li>
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/31d/cqglmf8wdf96n54i1sa2timget976xzz/2160_2160_1/AS1201A789750.webp'></img></li>
+        </ul>   
+      </section>  
+    )
+}
+
+export default Section;
