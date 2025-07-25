42795b11ea9385d862345b7235901e3d4d56e548
Damir <damircszar@mail.ru>
2025-07-25 20:01:10 +0300

    Разбил на компоненты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
 import Header from './Components/Header';
-
+import Mainpage from './Components/Mainpage';
 function App() {
   return (
+  <div>
     <Header />
+    <Mainpage />
+  </div>
   );
 }
 
--- src/Components/Mainpage.jsx
+++ src/Components/Mainpage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import './Mainpage.css';
 
-const Section = () => {
+const Mainpage = () => {
     return (
       <section className='content'>
         <ul className='snkrs-list'>
@@ -10,9 +11,12 @@ const Section = () => {
           <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/ec2/41fx71o654q98vfilmt0ks4aech705ce/2160_2160_1/NBU9060AAB.webp'></img></li>      
           <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/9a7/w219j30k5eoyp31303q4u8j8ocfbi5h4/2160_2160_1/NKHQ3818001.webp'></img></li>
           <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/31d/cqglmf8wdf96n54i1sa2timget976xzz/2160_2160_1/AS1201A789750.webp'></img></li>
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/386/gqlj2kiwr4328ng09uk93oeb1kbioueu/2160_2160_1/PTSTE06642.webp'></img></li>
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/c98/nt6llh0jhf3qa60nqwj046ll6uk1a1ru/2160_2160_1/NBU9060BLK.webp'></img></li>
+          <li className='snkr'><img className='shoes' src='https://superstep.ru/upload/resize_cache/iblock/035/fbyk827z386kq8ohes4au0scdi3ohq5t/2160_2160_1/PM40174404.webp'></img></li>
         </ul>   
       </section>  
     )
 }
 
-export default Section;
+export default Mainpage;
