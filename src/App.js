42a83a0b0a8d5a18bf72489a0b772896991dae4b
Damir <damircszar@mail.ru>
2025-08-02 18:18:11 +0300

    настроил маршрутизацию на вкладку новинки и бренды

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import { Header } from './Components/Header/Header';
 import { NewProducts } from './Components/NewProducts/NewProducts';
 import Brands from './Components/Brands/Brands';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <Header />
       <div className='content'>
-      <NewProducts />
-      {/*<Brands /> */}
+      <Routes>
+      <Route path='/NewProducts' element={<NewProducts />} />
+      <Route path='/Brands' element={<Brands />} />
+      </Routes>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,8 +7,8 @@ export const Header = () => {
           <div className={classes.menu}>  
              <img  className={classes.logo} src='https://www.nicepng.com/png/detail/66-667889_newbalance-comments-new-balance-shoe-icon.png'/>
               <ul className={classes.navlist}>      
-                <li><a className={classes.navlink}>Новинки</a></li>
-                <li><a className={classes.navlink}>Бренды</a></li>
+                <li><a href='/newproducts' className={classes.navlink}>Новинки</a></li>
+                <li><a href='/brands' className={classes.navlink}>Бренды</a></li>
                 <li><a className={classes.navlink}>Мужское</a></li>
                 <li><a className={classes.navlink}>Женское</a></li>
                 <li><a className={classes.navlink}>Аксессуары</a></li>
