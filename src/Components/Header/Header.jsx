b1d1effd68a6649e82a606fa29293349ba572a0f
Damir <damircszar@mail.ru>
2025-08-05 21:54:59 +0300

    Наполнил страницу Бренды, сделал подсвечивание активных ссылок, убрал подчеркивание и синий цвет посещенных ссылок

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,11 +8,11 @@ export const Header = () => {
           <div className={classes.menu}>  
              <img  className={classes.logo} alt='картинка' src='https://www.nicepng.com/png/detail/66-667889_newbalance-comments-new-balance-shoe-icon.png'/>
               <ul className={classes.navlist}>      
-                <li><NavLink to='/newproducts' className={classes.navlink}>Новинки</NavLink></li>
-                <li><NavLink to='/brands' className={classes.navlink}>Бренды</NavLink></li>
-                <li><NavLink to='/mens' className={classes.navlink}>Мужское</NavLink></li>
-                <li><NavLink to='/womens' className={classes.navlink}>Женское</NavLink></li>
-                <li><NavLink to='/accessories' className={classes.navlink}>Аксессуары</NavLink></li>
+                <li><NavLink to='/newproducts' activeClassName={classes.navlink}>Новинки</NavLink></li>
+                <li><NavLink to='/brands'  activeClassName={classes.navlink}>Бренды</NavLink></li>
+                <li><NavLink to='/mens'  activeClassName={classes.navlink}>Мужское</NavLink></li>
+                <li><NavLink to='/womens'  activeClassName={classes.navlink}>Женское</NavLink></li>
+                <li><NavLink to='/accessories'  activeClassName={classes.navlink}>Аксессуары</NavLink></li>
                 </ul>
             </div>
             
