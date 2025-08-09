04563566f06a35e66e4238328e0578c97e6ca5c1
Damir <damircszar@mail.ru>
2025-08-09 18:06:35 +0300

    активная страница всегда горит заданным цветом

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,11 +8,11 @@ export const Header = () => {
           <div className={classes.menu}>  
              <img  className={classes.logo} alt='картинка' src='https://www.nicepng.com/png/detail/66-667889_newbalance-comments-new-balance-shoe-icon.png'/>
               <ul className={classes.navlist}>      
-                <li><NavLink to='/newproducts' activeClassName={classes.navlink}>Новинки</NavLink></li>
-                <li><NavLink to='/brands'  activeClassName={classes.navlink}>Бренды</NavLink></li>
-                <li><NavLink to='/mens'  activeClassName={classes.navlink}>Мужское</NavLink></li>
-                <li><NavLink to='/womens'  activeClassName={classes.navlink}>Женское</NavLink></li>
-                <li><NavLink to='/accessories'  activeClassName={classes.navlink}>Аксессуары</NavLink></li>
+                <li><NavLink to='/newproducts' className={({isActive}) => isActive ? classes.activeLink : classes.link}>Новинки</NavLink></li>
+                <li><NavLink to='/brands'className={({isActive}) => isActive ? classes.activeLink : classes.link}>Бренды</NavLink></li>
+                <li><NavLink to='/mens' className={({isActive}) => isActive ? classes.activeLink : classes.link}>Мужское</NavLink></li>
+                <li><NavLink to='/womens'className={({isActive}) => isActive ? classes.activeLink : classes.link}>Женское</NavLink></li>
+                <li><NavLink to='/accessories' className={({isActive}) => isActive ? classes.activeLink : classes.link}>Аксессуары</NavLink></li>
                 </ul>
             </div>
             
--- src/Components/Mens/Mens.jsx
+++ src/Components/Mens/Mens.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import classes from './Mens.module.css'
+import { NavLink } from 'react-router-dom'
 
 
 const Mens = (props) => {
     return (
         <div className={classes.mens}>
-            Mens
+            <ul>
+                <li><NavLink to='/mens/1'>sneaker1</NavLink></li>
+                <li><NavLink to='/mens/2'>sneaker2</NavLink></li>
+                <li><NavLink to='/mens/3'>sneaker3</NavLink></li>
+            </ul>
         </div>
     )
 }
