e7903df0c5ce81c9b5ec3a70785336e0457ae043
Damir <damircszar@mail.ru>
2025-08-26 23:46:15 +0300

    массивы, метод массива map

--- src/Components/Mens/Mens.jsx
+++ src/Components/Mens/Mens.jsx
@@ -5,12 +5,25 @@ import { ItemCard } from './ItemCard/ItemCard'
 
 
 const Mens = (props) => {
+
+    let mensData = [
+        { id: 1, title: 'New Balance', subtitle: 'Кроссовки New Balance', img: 'https://superstep.ru/upload/resize_cache/iblock/f2d/y82r5rgjbazfz1bm3v2d0dnidw3q96gd/2160_2160_1/NBU9060ERA.webp' },
+        { id: 2, title: 'Nike', subtitle: 'Кроссовки Nike', img: 'https://superstep.ru/upload/resize_cache/iblock/11d/skmajh3dxqqmq8wingkjqen7p6xo6fxk/2160_2160_1/NKDV0833111.webp' },
+        { id: 3, title: 'Adidas', subtitle: 'Кроссовки Adidas', img: 'https://superstep.ru/upload/resize_cache/iblock/7ef/utdap6y4n10hz7weemw7d9k8ejelupfc/2160_2160_1/ADB75806R00.webp' }
+    ]
+
+    let mensElements = [
+        <NavLink to={"/Mens/" + mensData[0].id}><ItemCard title={mensData[0].title} subtitle={mensData[0].subtitle} img={mensData[0].img} id={mensData[0].id} /></NavLink>,
+        <NavLink to={"/Mens/" + mensData[1].id}><ItemCard title={mensData[1].title} subtitle={mensData[1].subtitle} img={mensData[1].img} id={mensData[1].id} /></NavLink>,
+        <NavLink to={"/Mens/" + mensData[2].id}><ItemCard title={mensData[2].title} subtitle={mensData[2].subtitle} img={mensData[2].img} id={mensData[2].id} /></NavLink>
+
+    ]
+
+
     return (
         <div className={classes.mens}>
             <ul className={classes.products}>
-                <NavLink to="/mens/1"> <ItemCard img="https://superstep.ru/upload/resize_cache/iblock/f2d/y82r5rgjbazfz1bm3v2d0dnidw3q96gd/2160_2160_1/NBU9060ERA.webp" title="New Balance" subtitle="Кроссовки New Balance" /> </NavLink>
-                <ItemCard img="https://superstep.ru/upload/resize_cache/iblock/f2d/y82r5rgjbazfz1bm3v2d0dnidw3q96gd/2160_2160_1/NBU9060ERA.webp" title="New Balance" subtitle="Кроссовки New Balance" />
-                <ItemCard img="https://superstep.ru/upload/resize_cache/iblock/f2d/y82r5rgjbazfz1bm3v2d0dnidw3q96gd/2160_2160_1/NBU9060ERA.webp" title="New Balance" subtitle="Кроссовки New Balance" />
+                {mensElements}
             </ul>
         </div>
     )
@@ -21,3 +34,12 @@ export default Mens;
 
 
 
+/* <NavLink to="/Mens/1"><ItemCard img="https://superstep.ru/upload/resize_cache/iblock/f2d/y82r5rgjbazfz1bm3v2d0dnidw3q96gd/2160_2160_1/NBU9060ERA.webp" title="New Balance" subtitle="Кроссовки New Balance" /></NavLink> 
+
+              <NavLink to={"/Mens/" + mensData[0].id}><ItemCard title={mensData[0].title} subtitle={mensData[0].subtitle} img={mensData[0].img} id={mensData[0].id} /></NavLink> 
+              <NavLink to={"/Mens/" + mensData[1].id}><ItemCard title={mensData[1].title} subtitle={mensData[1].subtitle} img={mensData[1].img} id={mensData[1].id}/></NavLink> 
+              <NavLink to={"/Mens/" + mensData[2].id}><ItemCard title={mensData[2].title} subtitle={mensData[2].subtitle} img={mensData[2].img} id={mensData[2].id} /></NavLink> 
+
+
+
+*/
--- src/Components/NewProducts/NewProducts.jsx
+++ src/Components/NewProducts/NewProducts.jsx
@@ -3,19 +3,38 @@ import classes from './NewProducts.module.css';
 import Comment from './Comments/Comment';
 
 export const NewProducts = (props) => {
+  
+  let newProductsData = [
+    {img:'https://superstep.ru/upload/resize_cache/iblock/9d3/iq70235n21xg7qja88zfj41ncwibmwhm/2160_2160_1/NKHJ4497400.webp'},
+    {img:'https://superstep.ru/upload/resize_cache/iblock/e73/kl7lkrah49y0iolkkqmeztrqnbmda0y8/2160_2160_1/AN8125177194.webp'},
+    {img:'https://superstep.ru/upload/resize_cache/iblock/d29/j4w2uddggv3xmjb32v26mqy0f4swya2h/2160_2160_1/NBU1906RCB.webp'},
+    {img:'https://superstep.ru/upload/resize_cache/iblock/ec2/41fx71o654q98vfilmt0ks4aech705ce/2160_2160_1/NBU9060AAB.webp'},
+    {img:'https://superstep.ru/upload/resize_cache/iblock/9a7/w219j30k5eoyp31303q4u8j8ocfbi5h4/2160_2160_1/NKHQ3818001.webp'},
+    {img:'https://superstep.ru/upload/resize_cache/iblock/31d/cqglmf8wdf96n54i1sa2timget976xzz/2160_2160_1/AS1201A789750.webp'},
+    {img:'https://superstep.ru/upload/resize_cache/iblock/386/gqlj2kiwr4328ng09uk93oeb1kbioueu/2160_2160_1/PTSTE06642.webp'},
+    {img:'https://superstep.ru/upload/resize_cache/iblock/c98/nt6llh0jhf3qa60nqwj046ll6uk1a1ru/2160_2160_1/NBU9060BLK.webp'},
+    {img:'https://superstep.ru/upload/resize_cache/iblock/035/fbyk827z386kq8ohes4au0scdi3ohq5t/2160_2160_1/PM40174404.webp'}
+
+  ]
+  
+  let commentData = [
+    {message:'Суперски', likesCount:'10' },
+    {message:'Здоровски', likesCount:'15' },
+    {message:'Топчик', likesCount:'20'}
+
+  ]
+  
+ 
+  let newProductsElements = newProductsData.map(Product => <ProductItem img={Product.img} /> ) ;
+ 
+  let commentElements = commentData.map(comment => <Comment message={comment.message} likesCount={comment.likesCount} /> ) ;
+  
+  
   return (
     <div>
       <section className='content'>
         <ul className={classes.snkrslist}>
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/9d3/iq70235n21xg7qja88zfj41ncwibmwhm/2160_2160_1/NKHJ4497400.webp' />
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/e73/kl7lkrah49y0iolkkqmeztrqnbmda0y8/2160_2160_1/AN8125177194.webp' />
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/d29/j4w2uddggv3xmjb32v26mqy0f4swya2h/2160_2160_1/NBU1906RCB.webp' />
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/ec2/41fx71o654q98vfilmt0ks4aech705ce/2160_2160_1/NBU9060AAB.webp' />
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/9a7/w219j30k5eoyp31303q4u8j8ocfbi5h4/2160_2160_1/NKHQ3818001.webp' />
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/31d/cqglmf8wdf96n54i1sa2timget976xzz/2160_2160_1/AS1201A789750.webp' />
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/386/gqlj2kiwr4328ng09uk93oeb1kbioueu/2160_2160_1/PTSTE06642.webp' />
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/c98/nt6llh0jhf3qa60nqwj046ll6uk1a1ru/2160_2160_1/NBU9060BLK.webp' />
-          <ProductItem img='https://superstep.ru/upload/resize_cache/iblock/035/fbyk827z386kq8ohes4au0scdi3ohq5t/2160_2160_1/PM40174404.webp' />
+         {newProductsElements}
         </ul>
       </section>
       <div>
@@ -24,9 +43,7 @@ export const NewProducts = (props) => {
         <button>Remove</button>
       </div>
       <div className={classes.comment}>
-        <Comment message='Суперски' likesCount='10' />
-        <Comment message='Здоровски' likesCount='15' />
-        <Comment message='Топчик' likesCount='20' />
+        {commentElements}
       </div>
 
 
@@ -53,10 +70,20 @@ const ProductItem = (props) => {
           <li className={classes.snkr}><img className={classes.shoes} src='https://superstep.ru/upload/resize_cache/iblock/c98/nt6llh0jhf3qa60nqwj046ll6uk1a1ru/2160_2160_1/NBU9060BLK.webp'></img></li>
           <li className={classes.snkr}><img className={classes.shoes} src='https://superstep.ru/upload/resize_cache/iblock/035/fbyk827z386kq8ohes4au0scdi3ohq5t/2160_2160_1/PM40174404.webp'></img></li> 
 
+        <Comment message={commentData[0].message} likesCount={commentData[0].likesCount} />
+        <Comment message={commentData[1].message} likesCount={commentData[1].likesCount} />
+        <Comment message={commentData[2].message} likesCount={commentData[2].likesCount} />
 
 
-
-
+          <ProductItem img={newProductsData[0].img} />
+          <ProductItem img={newProductsData[1].img} />
+          <ProductItem img={newProductsData[2].img} />
+          <ProductItem img={newProductsData[3].img}/>
+          <ProductItem img={newProductsData[4].img} />
+          <ProductItem img={newProductsData[5].img} />
+          <ProductItem img={newProductsData[6].img} />
+          <ProductItem img={newProductsData[7].img} />
+          <ProductItem img={newProductsData[8].img} />
 
 
 
